41e63be6227e7ea65e6e4f9e915b831ac5820da7
Hugge25 <111962456+Hugge25@users.noreply.github.com>
2023-11-16 20:45:26 +0100

    apple

---
+++ Applen och paron/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+int a = 7;
+int p = 13;
+
+string solda = Console.ReadLine();
+string[] frukter = solda.Split(' ');
+
+int[] totalt = Array.ConvertAll(frukter, int.Parse);
+int Axel = totalt[0]*a;
+int Petra = totalt[1]*p;
+
+if(Axel == Petra)
+    Console.WriteLine("lika");
+    
+else if(Axel > Petra)
+    Console.WriteLine("Axel");
+    
+else
+    Console.WriteLine("Petra");
