2a13cb491b2de0006359010ea3023104eb5825e4
Hugge25 <111962456+Hugge25@users.noreply.github.com>
2023-11-23 14:03:41 +0100

    bio

---
+++ Biosalong/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+bool run = true;
+int a = 0;
+int b = 0;
+
+int antal = int.Parse(Console.ReadLine()); //13
+
+string[] plaster = Console.ReadLine(); //#.####.###.##   ||   3
+
+Console.WriteLine(plaster[2]);
+
+while(run == true)
+{
+    
+}
